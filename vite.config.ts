3b350163d1653356ef4135d70a329d053bbccb9c
PaulMcManus23 <8432740+PaulMcManus23@users.noreply.github.com>
2026-07-08 08:36:47 +1000

    Add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/JsonSchemaBuilder/",
   plugins: [react()],
   resolve: {
-    dedupe: ['react', 'react-dom'],
+    dedupe: ["react", "react-dom"],
   },
-})
+});
